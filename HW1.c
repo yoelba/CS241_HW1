a9b4b14fd90946e1b37271660c718db678e1a945
Kristina Kolibab <tinytina228@yahoo.com>
2017-01-17 12:42:25 -0500

    Update HW1.c

--- HW1.c
+++ HW1.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char fileName[20]; // file we open and read from
+char newFileName[20]; // file we create and write to
+char str[25]; // buffered string being read in from file
+
 int main(){
+	
+	printf("Enter file name\n");
+	gets(fileName);
+	
 	FILE *inputFile; //A pointer to a file
-	inputFile = fopen("hollow.txt", "r"); //ALLOW USER TO INPUT FILE NAME IN FUTURE!
+	inputFile = fopen(fileName, "r");
+	
+	printf("Enter new file name\n");
+	gets(newFileName);
 	
 	FILE *outputFile;
-	outputFile = fopen("formattedText.txt", "w"); //ALLOW USER TO INPUT FILE NAME IN FUTURE!
-	
-	char myString[100]; //an array of characters. 
+	outputFile = fopen(newFileName, "w"); 
 
-	while(fscanf(inputFile, "%s", myString) != EOF){ //While I can read a complete word in from the file (IE, while file ain't even empty)
-		fputs(myString, outputFile);
+	while(fscanf(inputFile, "%s", str) != EOF){ //While I can read a complete word in from the file (IE, while file ain't even empty)
+		fputs(str, outputFile);
+		fputs(" ", outputFile); // so the new file includes spacing 
 	}
 	
 	fclose(inputFile);
